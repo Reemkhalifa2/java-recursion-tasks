8f078c2feef7aa761284cd8664dada8e62175c58
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-06 16:03:41 +0400

    recursion Case

--- src/ChangePi/ChangePi.java
+++ src/ChangePi/ChangePi.java
@@ -1,10 +1,18 @@
 package ChangePi;
 
 public class ChangePi {
-    public String ChangePi(String str){
+    public String changePi(String str){
 
         //Base Case
         if(str.isEmpty()) return "";
+
+        //recursion Case
+        return str.startsWith("pi") ?
+                "3.14" + changePi(str.substring(2))
+                :
+                str.charAt(0) +  changePi(str.substring(1));
+
+
 
     }
 }
