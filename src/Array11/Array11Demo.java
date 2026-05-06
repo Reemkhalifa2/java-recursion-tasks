30980901c9b8337a583654ab67a5ed0819dfc63b
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-06 16:15:38 +0400

    Object for Array11 class

--- src/Array11/Array11Demo.java
+++ src/Array11/Array11Demo.java
@@ -1,5 +1,13 @@
 package Array11;
 
 public class Array11Demo {
+    public static void main(String[] args) {
+
+        // Object for Array11 class
+        Array11 array11 = new Array11();
+
+
+
+    }
 
 }
