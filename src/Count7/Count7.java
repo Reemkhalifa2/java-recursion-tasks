cf79391c59f0dc119167d56e9eb37f9b90275e6d
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-06 15:44:44 +0400

    recursion case

--- src/Count7/Count7.java
+++ src/Count7/Count7.java
@@ -5,7 +5,15 @@ public class Count7 {
         if(n==0){
             return 0;//the base case
         }
+        //recursion case
+        int lastDigit = n % 10;
+        int remaining = n / 10;
 
+        if (lastDigit == 7) {
+            return 1 + count7(remaining);
+        } else {
+            return count7(remaining);
+        }
     }
 
 }
