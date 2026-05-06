629672ba68e15ab51f71e85a7991f5fac275d635
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-06 16:27:48 +0400

    Object for PairStar class

--- src/pairStar/PairStarDemo.java
+++ src/pairStar/PairStarDemo.java
@@ -1,4 +1,12 @@
 package pairStar;
 
 public class PairStarDemo {
+    public static void main(String[] args) {
+
+        //Object for PairStar class
+        PairStar pairStar = new PairStar();
+
+
+    }
+
 }
