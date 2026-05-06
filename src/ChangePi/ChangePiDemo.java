afacdf980f7ba1d37dd3341d1f4af91661a1978a
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-06 16:05:05 +0400

    Create an object of the ChangePi class

--- src/ChangePi/ChangePiDemo.java
+++ src/ChangePi/ChangePiDemo.java
@@ -3,5 +3,10 @@ package ChangePi;
 public class ChangePiDemo {
     public static void main(String[] args) {
 
+        // Object of changePI
+        ChangePi changePi = new ChangePi();
+
+
+
     }
 }
