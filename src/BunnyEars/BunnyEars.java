41610fd0877a19f197b95acfb8e694d7d51967f7
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-06 15:33:49 +0400

    define the recursion case

--- src/BunnyEars/BunnyEars.java
+++ src/BunnyEars/BunnyEars.java
@@ -5,6 +5,11 @@ public class BunnyEars {
         if(n ==0){
             return 0;//base case
         }
+        //recursion case
+        //if n is even bunny have 3 ears
+        //if n is odd bunny have 2 ears
+
+        return n%2==0 ? 3 + bunnyEars2(n - 1) :  2 + bunnyEars2(n - 1);
 
 
     }
