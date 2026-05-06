d506c412cbf63a5e7c4b96cd70a83696af4092cc
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-06 15:47:26 +0400

    Task 3: Create a Recursive Count7 Program in Java

--- src/Count7/Count7Demo.java
+++ src/Count7/Count7Demo.java
@@ -2,7 +2,13 @@ package Count7;
 
 public class Count7Demo {
     public static void main(String[] args) {
+
+        //object od count7 class
         Count7 count7 = new Count7();
-        count7.count7(717);
+
+        //Test
+        System.out.println(count7.count7(717));
+        System.out.println(count7.count7(7));
+        System.out.println(count7.count7(123));
     }
 }
