92377350d1c7c51b87b971badda547d1f2f19678
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-06 16:49:34 +0400

    Task 8: Create a Recursive CountAbc Program in Java

--- src/StrCount/StrCountDemo.java
+++ src/StrCount/StrCountDemo.java
@@ -5,7 +5,11 @@ public class StrCountDemo {
         // Object of StrCount
         StrCount strCount = new StrCount();
 
-        //
+        //Test
+        System.out.println(strCount.strCount("catcowcat", "cat"));
+        System.out.println(strCount.strCount("catcowcat", "cow"));
+        System.out.println(strCount.strCount("catcowcat", "dog"));
+
 
     }
 
