9022b57efc01cedc2928a809deff96fcd12d997f
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-06 16:46:31 +0400

    recursion case

--- src/StrCount/StrCount.java
+++ src/StrCount/StrCount.java
@@ -7,8 +7,10 @@ public class StrCount {
         //Base Case
         if (str.length()< sub.length()) return 0;
 
-
-
-
+        //recursion case
+        return str.startsWith(sub) ?
+                1 + strCount(str.substring(sub.length()), sub)
+                :
+                strCount(str.substring(1), sub);
     }
 }
