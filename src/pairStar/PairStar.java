abee1a7ae91bcf6c9fc043fb87f6c958c066d841
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-06 16:31:16 +0400

    fix bugs

--- src/pairStar/PairStar.java
+++ src/pairStar/PairStar.java
@@ -1,12 +1,14 @@
+package pairStar;
+
 public class PairStar {
     public String pairStar(String str){
 
         //Base Case
-        if(str.length() <=2) return str;
+        if(str.length() <=1) return str;
 
         //recursion case
         return str.charAt(0) == str.charAt(1) ?
-                str.charAt(0) + "*" + str.charAt(1)
+                str.charAt(0) + "*" + pairStar(str.substring(1))
                 :
                 str.charAt(0) + pairStar(str.substring(1));
 
