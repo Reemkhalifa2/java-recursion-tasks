ed65a4fa7619d85151cbabaa6db3ae6ccec37323
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-06 16:37:22 +0400

    recursion case

--- src/CountAbc.java
+++ src/CountAbc.java
@@ -4,6 +4,15 @@ public class CountAbc {
         // Base Case
         if(str.length() < 3) return 0;
 
+        // recursion case
+        return str.startsWith("abc")  || str.startsWith("aba") ?
+                1 + countAbc(str.substring(1))
+                :
+                countAbc(str.substring(1));
+
+
+
+
 
     }
 }
