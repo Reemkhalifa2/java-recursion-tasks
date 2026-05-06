4cdd034a0c9ed5a95f75cabca08346b675ab574f
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-06 15:20:16 +0400

    Call the factorial() method using the object and test it

--- src/Factorial.java
+++ src/Factorial.java
@@ -1,10 +1,10 @@
 public class Factorial {
 
     public int factorial(int n){
-        if (n==1){
+        if (n==1){ // base case
             return 1;
         }
-        return n * factorial(n-1);
+        return n * factorial(n-1);// recursive case
     }
 
 }
--- src/FactorialDemo.java
+++ src/FactorialDemo.java
@@ -1,9 +1,12 @@
 public class FactorialDemo {
     public static void main(String[] args) {
+        // object for factorial class
         Factorial factorial = new Factorial();
 
         //test
         System.out.println(factorial.factorial(3));
+        System.out.println(factorial.factorial(5));
+        System.out.println(factorial.factorial(10));
 
 
     }
