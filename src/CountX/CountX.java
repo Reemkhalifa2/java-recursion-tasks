9dadfbb53fa7e1caa1832fecbd65aa59a137b986
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-06 15:53:06 +0400

    recursion case

--- src/CountX/CountX.java
+++ src/CountX/CountX.java
@@ -4,6 +4,11 @@ public class CountX {
     public int countX(String str){
         if(str.isEmpty()) return 0; //base case
 
+        //recursion case
+        return str.charAt(0)== 'x' ?
+                (1 + countX(str.substring(1)))
+                :
+                countX(str.substring(1));
 
     }
 
