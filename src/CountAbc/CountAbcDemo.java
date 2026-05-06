c20cddda1e8c0150958d67b33841bdf4a45fd1e7
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-06 16:40:21 +0400

    object of countAbc class

--- src/CountAbc/CountAbcDemo.java
+++ src/CountAbc/CountAbcDemo.java
@@ -3,5 +3,10 @@ package CountAbc;
 public class CountAbcDemo {
     public static void main(String[] args) {
 
+        // object of countAbc
+        CountAbc countAbc = new CountAbc();
+
+
+
     }
 }
