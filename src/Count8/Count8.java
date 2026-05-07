7066b564f37de6a7db3d8914c929e45385f6da90
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-07 08:26:50 +0400

    Task 12: Create a Recursive Count8 Program in Java

--- src/Count8/Count8.java
+++ src/Count8/Count8.java
@@ -2,20 +2,21 @@ package Count8;
 
 public class Count8 {
 
-    public int Count8(int n){
+    public int count8(int n){
 
         //Base case
         if(n == 0) return 0;
 
         //recursion case
 
-        Integer lastDigit = n%10;
-        Integer secondLastDigit = (n/10) %10;
+        Integer lastDigit = n % 10;
+        Integer secondLastDigit = (n / 10) % 10;
         Integer remaining=  n / 10;
 
-        if (lastDigit == 8 &&secondLastDigit ==8) return 2 + Count8(remaining) ;
-        if (lastDigit == 8 || secondLastDigit ==8) return 1 + Count8(remaining);
-        return Count8(remaining);
+        if (lastDigit == 8 &&secondLastDigit ==8) return 2 + count8(remaining) ;
+        else if(lastDigit == 8) return 1 + count8(remaining);
+        else return count8(remaining);
+
 
 
 
--- src/Count8/Count8Demo.java
+++ src/Count8/Count8Demo.java
@@ -1,5 +1,14 @@
 package Count8;
 
 public class Count8Demo {
+    public static void main(String[] args) {
+        //Object of Count 8
+        Count8 count8 = new Count8();
+
+        // Test
+        System.out.println(count8.count8(8));
+        System.out.println(count8.count8(818));
+        System.out.println(count8.count8(8818));
+    }
 
 }
