f3fae4765bc5e900d8b8fc4f539a5ebdd7f186a4
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-07 08:48:40 +0400

    recursion case

--- src/EndX/EndX.java
+++ src/EndX/EndX.java
@@ -6,6 +6,12 @@ public class EndX {
         //Base case
         if(str.length() == 0) return "";
 
+        //recursion case
+        return str.charAt(0)== 'x' ?
+                endX(str.substring(1)) + "x"
+                :
+                str.charAt(0) + endX(str.substring(1)) ;
+
     }
 
 }
