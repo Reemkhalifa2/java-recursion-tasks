4503db37568bb62d9453b3af68653e6a4539c76e
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-07 08:56:29 +0400

    create ParenBit class

---
+++ src/Count11/Count11Demo.java
@@ -0,0 +1,13 @@
+package Count11;
+
+public class Count11Demo {
+    public static void main(String[] args) {
+        // object of count11 class
+        Count11 count11 = new Count11();
+
+        //Test
+        System.out.println(count11.count11("11abc11"));
+        System.out.println(count11.count11("abc11x11x11"));
+        System.out.println(count11.count11("111"));
+    }
+}
---
+++ src/ParenBit/ParenBit.java
@@ -0,0 +1,5 @@
+package ParenBit;
+
+public class ParenBit {
+
+}
