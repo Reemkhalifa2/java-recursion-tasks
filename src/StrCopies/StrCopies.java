a8ea5c3760e8d066385898d982b62e94ee49a901
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-07 09:22:00 +0400

    define base case

--- src/StrCopies/StrCopies.java
+++ src/StrCopies/StrCopies.java
@@ -3,6 +3,13 @@ package StrCopies;
 public class StrCopies {
     public Boolean  strCopies(String str, String sub, int n){
 
+        //base case
+        if(n==0) return true;
+        if(str.length()< sub.length()) return false;
+
+
+
+
     }
 
 }
