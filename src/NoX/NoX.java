8dca03457c15ad3fcc691bd79b65e900f81f8876
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-07 08:41:12 +0400

    recursion case

--- src/NoX/NoX.java
+++ src/NoX/NoX.java
@@ -1,4 +1,19 @@
 package NoX;
 
 public class NoX {
+    public String noX(String str){
+
+        //Base case
+        if(str.length() == 0) return "";
+
+        //recursion case
+        return  str.charAt(0) == 'x' ?
+                noX(str.substring(1))
+                :
+                str.charAt(0) + noX(str.substring(1));
+
+
+
+
+    }
 }
