d569e9419891dcb917df4d31547d82a0989a1d0d
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-07 08:50:01 +0400

    Task 15: Create a Recursive EndX Program in Java

---
+++ src/EndX/EndXDemo.java
@@ -0,0 +1,13 @@
+package EndX;
+
+public class EndXDemo {
+    public static void main(String[] args) {
+        // object of EndX
+        EndX endX = new EndX();
+
+        // Test
+        System.out.println(endX.endX("xxre"));
+        System.out.println(endX.endX("xxhixx"));
+        System.out.println(endX.endX("xhixhix"));
+    }
+}
