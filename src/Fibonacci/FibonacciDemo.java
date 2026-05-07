cbfc81c780f078eb7c1335b5c9775938128a1436
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-07 09:30:13 +0400

    Task 19: Create a Recursive Fibonacci Program in Java

---
+++ src/Fibonacci/FibonacciDemo.java
@@ -0,0 +1,13 @@
+package Fibonacci;
+
+public class FibonacciDemo {
+    public static void main(String[] args) {
+        // Object of Fibonacci class
+        Fibonacci fibonacci = new Fibonacci();
+
+        // Test
+        System.out.println(fibonacci.fibonacci(0));
+        System.out.println(fibonacci.fibonacci(1));
+        System.out.println(fibonacci.fibonacci(2));
+    }
+}
