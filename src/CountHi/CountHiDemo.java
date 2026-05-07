301a6e609cce47b4ff3020c9eddd3a7326c25646
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-07 08:32:55 +0400

    Create an object of the CountHi class

--- src/CountHi/CountHiDemo.java
+++ src/CountHi/CountHiDemo.java
@@ -3,6 +3,10 @@ package CountHi;
 public class CountHiDemo {
     public static void main(String[] args) {
 
+        //Object of CountHI class
+        CountHi countHi = new CountHi();
+
+
     }
 
 }
