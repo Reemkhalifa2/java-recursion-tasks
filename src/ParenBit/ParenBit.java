06618b6f9dff69c98bf3c9e6cb6ff984da5dec20
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-07 08:58:02 +0400

    define base case

--- src/ParenBit/ParenBit.java
+++ src/ParenBit/ParenBit.java
@@ -1,5 +1,13 @@
 package ParenBit;
 
 public class ParenBit {
+    public String  parenBit(String str){
+
+        //Base case
+        if(str.startsWith("(") && str.endsWith(")")) return  str;
+    }
+
+
+
 
 }
