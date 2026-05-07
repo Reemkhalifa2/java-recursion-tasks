83c3d19f016a70d2e6b51f78c7e05de4745f1ad5
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-07 09:28:34 +0400

    define recursion cases

--- src/Fibonacci/Fibonacci.java
+++ src/Fibonacci/Fibonacci.java
@@ -3,6 +3,14 @@ package Fibonacci;
 public class Fibonacci {
     public int  fibonacci(int n){
 
+        //base cases
+        if(n==0) return 0;
+        if(n==1) return 1;
+
+        // recursion case
+        return  fibonacci(n - 1) + fibonacci(n - 2);
+        
+
     }
 
 }
