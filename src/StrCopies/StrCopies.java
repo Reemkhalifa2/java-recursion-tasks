62f48b274aa33c1c27ce3c6634f50a5208086003
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-07 09:23:11 +0400

    define recursion case

--- src/StrCopies/StrCopies.java
+++ src/StrCopies/StrCopies.java
@@ -7,6 +7,12 @@ public class StrCopies {
         if(n==0) return true;
         if(str.length()< sub.length()) return false;
 
+        // recursion case
+        return str.substring(0, sub.length()).equals(sub) ?
+                strCopies(str.substring(1), sub, n - 1)
+                :
+                strCopies(str.substring(1), sub, n);
+
 
 
 
