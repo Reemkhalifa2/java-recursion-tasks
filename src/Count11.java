431363dc3776d907f36c1d555d3873410ae3fb33
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-07 08:53:58 +0400

    recursion case

--- src/Count11.java
+++ src/Count11.java
@@ -3,6 +3,13 @@ public class Count11 {
         // Base case
         if (str.length() < 2) return 0;
 
+        //recursion case
+        return str.startsWith("11") ?
+                1 + count11(str.substring(2))
+                :
+                count11(str.substring(1));
+
+
     }
 
 }
