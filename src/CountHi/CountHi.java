5d2db148995c6b35e60ac5eb3c0bd7afea242475
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-07 08:31:41 +0400

    recursion case

--- src/CountHi/CountHi.java
+++ src/CountHi/CountHi.java
@@ -6,7 +6,11 @@ public class CountHi {
         //Base Case
         if(str.length() < 2) return 0;
 
-        //
+        //recursion case
+        return str.substring(0, 2).equals("hi")?
+                1 + countHi(str.substring(2))
+                :
+                countHi(str.substring(1));
 
 
     }
