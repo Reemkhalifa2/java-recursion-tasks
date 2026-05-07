77c1926ff6030bc7b8552fe02abaea5e86808491
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-07 08:10:06 +0400

    Object of Triangle class

--- src/Triangle/TriangleDemo.java
+++ src/Triangle/TriangleDemo.java
@@ -3,5 +3,9 @@ package Triangle;
 public class TriangleDemo {
     public static void main(String[] args) {
 
+        //Object of Triangle class
+        Triangle triangle = new Triangle();
+
+
     }
 }
