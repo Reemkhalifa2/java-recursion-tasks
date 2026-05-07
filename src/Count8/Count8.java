6aa7339c6f267414457cb3fcf4617cc3b9522151
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-07 08:18:25 +0400

    recursion case

--- src/Count8/Count8.java
+++ src/Count8/Count8.java
@@ -7,6 +7,19 @@ public class Count8 {
         //Base case
         if(n == 0) return 0;
 
+        //recursion case
+
+        Integer lastDigit = n%10;
+        Integer secondLastDigit = (n/10) %10;
+        Integer remaining=  n / 10;
+
+        if (lastDigit == 8 &&secondLastDigit ==8) return 2 + Count8(remaining) ;
+        if (lastDigit == 8 || secondLastDigit ==8) return 1 + Count8(remaining);
+        return Count8(remaining);
+
+
+
+
 
 
     }
